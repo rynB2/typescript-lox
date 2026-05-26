7762b1e6904fbcef21285bbdf662f168fbf2c8cc
Ryan Leslie <ryan.leslie@designlogica.co.uk>
2026-05-26 02:33:56 +0100

    finish scanner

--- Scanner.ts
+++ Scanner.ts
@@ -19,6 +19,56 @@ export const createScanner = (source: string): Scanner => {
 
   const advance = (): string => source.charAt(current++);
 
+  const peek = (): string => {
+    if (isAtEnd()) return "\0";
+    return source.charAt(current);
+  };
+
+  const match = (expected: string): boolean => {
+    if (isAtEnd()) return false;
+    if (source.charAt(current) != expected) return false;
+    current++;
+    return true;
+  };
+
+  const string = (): void => {
+    while (peek() != '"' && !isAtEnd()) {
+      if (peek() == "\n") {
+        // handle multi-line string edge case
+        line++;
+      }
+      advance();
+    }
+
+    if (isAtEnd()) {
+      errorReporter.error(line, "Unterminated string.");
+      return;
+    }
+
+    // the closing ".
+    advance();
+
+    // trim the surrounding quotes
+    const value = source.substring(start + 1, current - 1);
+    addToken(TokenType.STRING, value);
+  };
+
+  const number = (): void => {
+    while (isDigit(peek())) advance();
+
+    // look for a fractional part
+    if (peek() == "." && isDigit(peekNext())) {
+      // consume the "."
+      advance();
+      while (isDigit(peek())) advance();
+    }
+
+    const text = addToken(
+      TokenType.NUMBER,
+      Number(source.substring(start, current)),
+    );
+  };
+
   const addToken = (type: TokenType, literal: unknown = null): void => {
     const text = source.substring(start, current);
     tokens.push({ type: type, lexeme: text, literal: literal, line: line });
